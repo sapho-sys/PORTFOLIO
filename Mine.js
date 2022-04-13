df40596296c8c627a28f522c49f8017569ee4aa2
sapho-sys <saphonkunzi@gmail.com>
2022-04-13 10:27:20 +0200

    new updates

--- Mine.js
+++ Mine.js
@@ -1,50 +1,6 @@
 
-////this block of code is for my corousel
-
-var slideIndex = 0;
-showSlides(slideIndex);
-//this functions moves from one index of the page to another
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-//this function is where all the operation takes place
-function showSlides(n) {
-  var i;
-
-//we fetch the div elements by class from html assigned to new variables
-  var slides = document.getElementsByClassName("mySlides");
-
-  var dots = document.getElementsByClassName("demo");
-
-  var captionText = document.getElementById("caption");
 
 
-//l
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-//
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  //display
-  slides[slideIndex-1].style.display = "block";
-
-  dots[slideIndex-1].className += " active";
-  
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
 
 //this code is for my modal
 var modal = document.getElementById("myModal");
